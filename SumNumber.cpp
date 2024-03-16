dd7e0e6444109de7773df273f4b6f81334a13b81
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-16 18:51:16 +0530

    sum of 1 to n number

---
+++ SumNumber.cpp
@@ -0,0 +1,18 @@
+/*sum fom 1 to an upperbond using a while .loop (sumNumber.cpp)*/
+#include<iostream>
+using namespace std;
+int main(){
+  int upperbond;
+  int sum=0;//sum form  1 upperbond
+  cout<<"Enter the number of upper bond: ";
+  cin>>upperbond;
+  int number=1;
+  while( number<=upperbond){
+    sum=sum+number;
+++number;
+
+
+  }
+  cout<<"The  sum from 1 to "<<upperbond<<"is"<<sum<<endl;
+  return 0;
+}
