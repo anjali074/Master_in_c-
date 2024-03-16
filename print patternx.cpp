cf8ac39f835460ac9c6c41ba0c538eb2fb7e7c76
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-16 17:58:57 +0530

    pattern

---
+++ print patternx.cpp
@@ -0,0 +1,9 @@
+/*first c++ program that say (pint pattern.cpp)*/
+#include<iostream>// needed program io operation
+using namespace std;
+int main(){//program entry point
+cout<<"* * * * * *\n * * * * * *\n* * * * * \n * * * * *\n* * * * *\n     (a)   "<<endl;
+cout<<"* * * * * *\n *      *\n*       * \n *        *\n* * * * * *\n      (b)   "<<endl;
+cout<<"* * * * * *\n  *        *  \n   *     *  \n *   *\n    *     \n     (c)   "<<endl;
+return 0;
+}
