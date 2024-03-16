3314d3c9be9433047c2427c12660c6e84330fa9e
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-16 17:44:32 +0530

    hello basic terminated

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+/*first c++ program that say hello9hello.cpp)*/
+#include<iostream>// needed program io operation
+using namespace std;
+int main(){//program entry point 
+  cout<<"hellow,world"<<endl;//say hello
+  return 0;//terminated main
+}//end of main function
