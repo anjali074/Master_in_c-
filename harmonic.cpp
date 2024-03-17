9e1a2103c3fbba349d7a63f248b6f95580724fb6
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-17 18:20:46 +0530

    denominstor

---
+++ harmonic.cpp
@@ -0,0 +1,15 @@
+/*sum harmonics series*/
+
+#include<iostream>
+using namespace std;
+int main(){
+  int maxDenomintor;//sum in "int"
+  double sum=0;
+  int denominator=1;
+  while(denominator<=maxDenomintor){
+    ++denominator;
+  }
+  //print sum
+  cout<<"Sum of Harmonic Series is : "<<sum<<endl;
+  cout<<"sum of denominator is"<<denominator<<endl;
+}
