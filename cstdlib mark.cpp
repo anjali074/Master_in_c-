f589ada82c0e795591d9448771c4313fc2764a86
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-18 17:39:19 +0530

    marks

---
+++ cstdlib mark.cpp
@@ -0,0 +1,14 @@
+/** Recommended  programming style in mark of student*/
+#include<iostream>
+using namespace std;
+int main(){
+  int  mark=70;
+  if(mark>=50){
+    cout<<"you pass!"<<endl;
+
+  }
+  else{
+    cout<<"you fail!"<<endl;
+    return 0;
+  }
+}
