66c3b00ce7c247f59da4b84be640e3d730f5b15e
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-18 18:56:38 +0530

    escap

---
+++ escaspe.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+int main(){
+  cout<<"               '    ' ";
+  cout<<"               ____\n";
+  cout<<"               (0  0)\n";
+  cout<<"      +=========\\  / \n    /||  % % %  || \n   * ||- - - - -|| \n     \"\"         \"\" "<<endl;
+  return 0;
+}
