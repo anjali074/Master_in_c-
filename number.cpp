3c734bd7e8fb0efc582e73044530223e7df4ed5e
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-18 17:46:15 +0530

    print garbage value

---
+++ number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<n<<endl;
+  return 0; //it contain variable return garbage value assigen and intile value /does not issue warning /error you use a variable before intial;
+
+}
