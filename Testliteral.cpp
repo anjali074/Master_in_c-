d0ba09c2f3cbbf1530aca89cc87fb29edd5cd727
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-18 18:26:43 +0530

    testliteral

---
+++ Testliteral.cpp
@@ -0,0 +1,20 @@
+/*Testing  primitive Type*/
+#include<iostream>
+using namespace std ;
+int main(){
+  char gender='m';
+  bool isMarried=true;
+  unsigned short numchildren=8;
+  short yearofbirth=194;
+  unsigned intsalary=88000;
+  double weight=88.88;
+  float gpa=3.88f;
+  cout<<"Gender is"<<gender<<endl;
+  cout<<"is Married is"<<isMarried<<endl;
+   cout<<"Number of children:"<<numchildren<<endl;
+   cout<<"Year of Birth:"<<yearofbirth<<endl;
+   cout<<"weight"<<weight<<endl;
+   cout<<"Gpa"<<gpa<<endl;
+   return 0;
+
+}
