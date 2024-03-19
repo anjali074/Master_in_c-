18c39a656d2c224a37dae94ba0ca57aa465fd5cf
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-03-19 19:53:16 +0530

    incement and decrement opreator

---
+++ Testin.Dec.cpp
@@ -0,0 +1,20 @@
+/* test on increment (++) and decrement(--) operator */
+#include<iostream>
+using namespace std;
+int main(){
+  int mark=76;
+  cout<<mark<<endl;
+  mark++;
+  cout<<mark<<endl;
+  ++mark;
+  cout<<mark<<endl;
+  mark=mark+1;
+  cout<<mark<<endl;
+  mark--;
+  cout<<mark<<endl;
+  --mark;
+  cout<<mark<<endl;
+  mark=mark-1;
+  cout<<mark<<endl;
+  return 0;
+}
