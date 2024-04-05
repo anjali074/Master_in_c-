bbc4b220413a607e0e21f48896fa76893714ab38
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-05 16:14:44 +0530

    flow control conditional loop

---
+++ flowcontrol.cpp
@@ -0,0 +1,32 @@
+// if-then conditional decision
+#include<iostream>
+using namespace std;
+int main(){
+  /*int mark;
+  if(mark>=50){
+    cout<<"congratulation"<<endl;
+    cout<<"keep it up"<<endl;*/
+
+    //if-then-else
+    int mark;
+    /*if(mark>=50){
+      cout<<"congratulation"<<endl;
+    }
+    else{
+    cout<<"keep it up"<<endl;*/
+
+//nested if-then-else
+if(mark>=80){
+   cout<<"A"<<endl;
+}
+else if(mark>=70){
+  cout<<"B"<<endl;
+}
+else if(mark>=60){
+  cout<<"C"<<endl;
+}
+else{
+  cout<<"D"<<endl;
+}
+
+}
