2331d2da3b48ca609e8f695c3a3471f4066074d4
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-05 16:48:36 +0530

    conditional loop

--- conditionallopp.cpp
+++ conditionallopp.cpp
@@ -3,8 +3,14 @@
 #include<iostream>
 using namespace std;
 int main(){
-int sum=0;
-for(int number=1; number<=1000;++number ){
+int sum=0, number=1;
+/*for(int number=1; number<=1000;++number ){
   sum+=number;
+}*/
+//while-l00p
+while(number <= 1000){
+  sum+=number;
+  ++number;
+  cout<<"sum is"<<sum<<endl;
 }
 }
