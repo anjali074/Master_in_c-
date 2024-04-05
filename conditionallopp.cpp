b2b381c91d7b489b8de83a5dc2018156fbc2190e
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-05 16:37:42 +0530

    loop conditional

---
+++ conditionallopp.cpp
@@ -0,0 +1,10 @@
+/*loop will be executed multiple time
+*1. for-loop print sum of number*/
+#include<iostream>
+using namespace std;
+int main(){
+int sum=0;
+for(int number=1; number<=1000;++number ){
+  sum+=number;
+}
+}
