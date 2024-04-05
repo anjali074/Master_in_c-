aeb0c439314813c965127544c9542c5b3aa6b365
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-05 17:04:05 +0530

    loop

--- conditionallopp.cpp
+++ conditionallopp.cpp
@@ -1,16 +1,16 @@
 /*loop will be executed multiple time
-*1. for-loop print sum of number*/
+*1. do-while print sum of number*/
 #include<iostream>
 using namespace std;
 int main(){
 int sum=0, number=1;
-/*for(int number=1; number<=1000;++number ){
-  sum+=number;
-}*/
-//while-l00p
-while(number <= 1000){
+do{
+  if(number>=1&&number<=50){ //condition to check the
   sum+=number;
   ++number;
-  cout<<"sum is"<<sum<<endl;
+  
 }
+while(number<=1000){
+    cout<<"Sum is : "<<sum<<endl;
+  }
 }
