5b81174b7b1e7783f3f3e12b9ca7a9a2b413b712
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-07 23:37:06 +0530

    first function

---
+++ function1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int power(int a, int b);
+    int ans =1;
+
+    for(int i=1; i<=ans; i++){
+      ans=ans*i;
+    }
+    return ans;
+  }
+
