1b4d2c741f61a67696b627e6b01c8b36a512380d
Anjali Kumari <113256438+anjali074@users.noreply.github.com>
2024-04-07 22:59:27 +0530

    switch & function

---
+++ switch statement.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int num=2;
+  cout<<endl;
+  switch(num){
+    case 1: cout<<"first ";
+    break;
+    case 2: cout<<"second ";
+    break;
+    default:cout<<"it default case ";
+  }
+}
